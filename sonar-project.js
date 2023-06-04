8933258144e5198041839e3db59b32dc40eae230
irfan313pathan <128775354+irfan313pathan@users.noreply.github.com>
2023-06-04 10:45:17 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.223.61:9000/',
+  serverUrl: 'http://13.127.129.202:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
